97dffae82241e3d048a31b43e97007581bd434c5
Felipe Petriw <felipe.petriw.ads@gmail.com>
2023-07-31 15:48:15 -0300

    Input Range
    Alteração de dados ao manipular o range.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
+let passwordLength = 16
+
 function generatePassword() {
     const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ123456789?!@&*()[]"
  
     let password = ""
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         const randonNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randonNumber, randonNumber + 1)
 
         const inputEl = document.querySelector("#password")
         inputEl.value = password
-
-        console.log(password)
     }
 }
+
+const passwordLengthEl = document.querySelector("#password-length")
+passwordLengthEl.addEventListener("input", function () {
+    const passwordLength = passwordLengthEl.value
+    generatePassword()
+})
+
 generatePassword()
